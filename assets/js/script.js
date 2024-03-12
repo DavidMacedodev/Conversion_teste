114bcb4bfb6eace90f340ca4847f575214d2ff5c
David Macedo <newlima159@gmail.com>
2024-03-11 22:01:48 -0300

    feat: search cats by name and id

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ const apiKey = 'live_6ViJij85dNfCko6PJcsb6Do1dFAkCmTteN0nxQcbOI40VKzDwRujH7hizNc
 const catImg = document.getElementById('cat-img-1');
 const catImg2 = document.getElementById('cat-img-2');
 
-const spanMSGs = document.querySelector('.inputMessage');
+const errorMSGs = document.querySelector('.inputMessage')
 const form = document.getElementById('search-form');
 
 const input = document.getElementById('input-search');
@@ -17,26 +17,51 @@ const fetchBreedById = async (breedId) => {
     }
 }
 
-const renderCat = async (breedId) => {
-    spanMSGs.textContent = 'Loading...';
+const fetchBreedByName = async (breedName) => {
+    const APIResponse = await fetch(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`);
+
+    if (APIResponse.status == 200) {
+        const data = await APIResponse.json();
+        if (data.length > 0) {
+            return data[0].id; 
+        }
+    }
+    return null;
+}
+
+const renderCat = async (breed) => {
+    errorMSGs.textContent = 'Loading...'
+
+    let breedId = breed;
+  
+        breedId = await fetchBreedByName(breed);
+        if (!breedId) {
+            errorMSGs.textContent = "Raça não encontrada, digite um nome ou ID de raça válido";
+            catImg.src = '';
+            catImg2.src = '';
+            return;
+        }
+    
 
     const data = await fetchBreedById(breedId);
 
     if (data && data.length > 0) {
-        spanMSGs.textContent = ''
+        errorMSGs.textContent = ''
         catImg.src = data[0].url;
         catImg2.src = data[1].url;
 
         input.value = ''
     } else {
-        spanMSGs.textContent = "ID não encontrado, digite um ID válido"
+        errorMSGs.textContent = "ID não encontrado, digite um ID válido"
     }
+
 }
 
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
     await renderCat(input.value.toLowerCase())
+    
 })
 
 renderCat('cypr');
